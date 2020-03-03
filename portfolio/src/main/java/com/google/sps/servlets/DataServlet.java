d9eeaa72c0bda63da3cb038670a6fd120f180b24
Jorge Daboub <jjdaboub1@gmail.com>
2020-03-03 00:05:07 -0500

    Add hardcoded comments display

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,9 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import com.google.gson.Gson;
+import java.util.List;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,9 +27,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+    private List<String> comments;
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Jorge!</h1>");
+    comments = new ArrayList<>();
+    comments.add("Awesome website keep up the good work!. - Sundar Pichai");
+    comments.add("I liked your picture. - Maria Sharapova");
+    comments.add("Wanna hang out some time? - Jorge Mario Bergoglio");
+    
+    String json = convertToJson(comments);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+
+    //response.setContentType("text/html;");
+    //response.getWriter().println("<h1>Hello Jorge!</h1>");
+  }
+
+  private String convertToJson(List<String> comments) {
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+    return json;
   }
 }
